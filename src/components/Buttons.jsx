c241f2ae4fa645e0adcace7de58efaa0bf509477
Princeton Arulnesan <princeton.arul@gmail.com>
2020-08-21 20:32:00 +0000

    added logos

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import './Buttons.css';
+import LinkedIn from '../Image/linkedin-logo.png';
+import Github from '../Image/GitHub-logo.png';
+import Gmail from '../Image/gmail-logo.png';
+import Facebook from '../Image/facebook-logo.png';
 
 export default function Buttons(props) {
   return (
-    <section>
-      <button>LinkedIn</button>
-      <button>Github</button>
-      <button>Email</button>
-      <button>Facebook</button>
+    <section className="socials">
+      <button type="submit" onclick="location.href='www.linkedin.com/in/princeton-arulnesan';">
+        <img src={LinkedIn} alt="LinkedIn">
+        </img>
+      </button>
+      <button>
+        <img src={Github} alt="Github">
+        </img>
+        </button>
+      <button>
+        <img src={Gmail} alt="Email">
+        </img>
+        </button>
+      <button>
+        <img src={Facebook} alt="Facebook">
+        </img>
+        </button>
     </section>
   )
 }
