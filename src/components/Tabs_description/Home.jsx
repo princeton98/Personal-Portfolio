ef3e741d56fec4db628b114f8fc477b11f4ba22a
Princeton Arulnesan <princeton.arul@gmail.com>
2020-09-21 19:26:47 +0000

    fixed up a few things in code

--- src/components/Tabs_description/Home.jsx
+++ src/components/Tabs_description/Home.jsx
@@ -5,9 +5,9 @@ export default function Home(props) {
     <div>
       <h1>About me</h1>
       <h5>Subtext</h5>
+        <h3 className="text-description">Newly trained web developer</h3>
       <section className="text">
       <div className="left-side">
-        <h3>Newly trained web developer</h3>
         <p>
           Began a direction towards Psychology, but decided to instead pursue coding and web development through a 12-week intensive bootcamp designed by Lighthouse Labs. Learned useful tech and techniques such as React and Node.js that are vital towards the web development job.
         </p>
